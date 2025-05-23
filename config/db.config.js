21f3eea1e022e12cccae8ecf92e5f5be3e9e22bc
Thandov <thandov.hlophe@gmail.com>
2025-05-23 12:39:43 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: process.env.DB_HOST || "kayiseit.co.za",
+  HOST: process.env.DB_HOST || "92.205.15.114",
   USER: process.env.DB_USER || "mrt",
   PASSWORD: process.env.DB_PASS || "Wh@tGu3ss@@2025",
   DB: process.env.DB_NAME || "mygigguide",
