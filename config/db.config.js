0f8289b36801dc2c5d42c57d2a27b674ec20c0d0
Thandov <49472028+Thandov@users.noreply.github.com>
2025-05-23 12:14:15 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,8 @@
-/*File location: 
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root", // or your actual password
-    DB: "my_gig_guide_db",
-    dialect: "mysql",
-  };
-*/
 module.exports = {
   HOST: process.env.DB_HOST,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASS,
   DB: process.env.DB_NAME,
   PORT: process.env.DB_PORT || 3306,
+  dialect: "mysql",
 };
