b1c58c79471e7aacc28c48b73570f26450858f23
Thandov <thandov.hlophe@gmail.com>
2025-05-23 12:27:54 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -3,6 +3,6 @@ module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "root", // or your actual password
-    DB: "my_gig_guide_db",
+    DB: "mygigguide",
     dialect: "mysql",
   };
