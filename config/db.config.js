368233faddc40d9589b3b6bf3058a18dbbae6b31
Thandov <49472028+Thandov@users.noreply.github.com>
2025-05-25 17:46:28 +0200

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,8 @@
 //File location: 
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root", // or your actual password
-    DB: "my_gig_guide_db",
-    dialect: "mysql",
-  };
+  HOST: process.env.DB_HOST || "92.205.15.114",
+  USER: process.env.DB_USER || "mrt",
+  PASSWORD: process.env.DB_PASS || "Wh@tGu3ss@@2025",
+  DB: process.env.DB_NAME || "mygigguide",
+  dialect: "mysql",
+};
